a010eeda9cdbdb15f5f0a39125c0d2eddb604168
Karine Ribeiro <karine.yasmin@outlook.com>
2025-11-10 18:35:55 -0300

    feat: add filtro de vendedores

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ from graphs import (
 st.set_page_config(layout="wide")
 st.title("Dashboard de Vendas :shopping_cart:")
 
+st.sidebar.title("Filtro de Vendedores")
+filtro_vendedor = st.sidebar.multiselect("Vendedores", df["Vendedor"].unique())
+
+if filtro_vendedor:
+    df = df[df["Vendedor"].isin(filtro_vendedor)]
+
 aba1, aba2, aba3 = st.tabs(["Dataset", "Receita", "Vendedores"])
 
 with aba1:
