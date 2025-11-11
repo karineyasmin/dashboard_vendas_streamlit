f303c45e723568916984883152f3174cc12cf708
Karine Ribeiro <karine.yasmin@outlook.com>
2025-11-10 22:26:58 -0300

    feat: add queries e contagem de linhas e colunas

--- pages/dataframe.py
+++ pages/dataframe.py
@@ -1,24 +1,32 @@
 import streamlit as st
 from dataset import df
 
-st.title("Dataset) de Vendas")
+st.title("Dataset de Vendas")
 with st.expander("Colunas"):
     colunas = st.multiselect("Selecione as Colunas", list(df.columns), list(df.columns))
-
 st.sidebar.title("Filtros")
 with st.sidebar.expander("Categoria do Produto"):
     categorias = st.multiselect(
         "Selecione as categorias",
         df["Categoria do Produto"].unique(),
-        "Selecione as categorias",
         df["Categoria do Produto"].unique(),
     )
 with st.sidebar.expander("Preço do Produto"):
     preco = st.slider("Selecione o Preço", 0, 5000, (0, 5000))
-
 with st.sidebar.expander("Data da Compra"):
     data_compra = st.date_input(
-        "Selecione a data", df["Data da Compra"].min(), df["Data da Compra"].max()
+        "Selecione a data", (df["Data da Compra"].min(), df["Data da Compra"].max())
     )
 
-st.dataframe(df)
+query = """
+    `Categoria do Produto` in @categorias and \
+    @preco[0] <= Preço  <= @preco[1] and \
+    @data_compra[0] <= `Data da Compra` <= @data_compra[1]
+"""
+filtro_dados = df.query(query)
+filtro_dados = filtro_dados[colunas]
+
+st.markdown(
+    f"A tabela possui :blue[{filtro_dados.shape[0]}] linhas e :blue[{filtro_dados.shape[1]}] colunas"
+)
+st.dataframe(filtro_dados)
